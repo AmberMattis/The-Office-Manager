5caa267f0755e5c3ae492a76b2ffed9d02094478
Amber Mattis <amber.mattis2@gmail.com>
2021-02-13 20:37:49 -0600

    created home controller, post model, and home view

---
+++ src/main/java/com/example/theofficemanager/models/Post.java
@@ -0,0 +1,11 @@
+package com.example.theofficemanager.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
