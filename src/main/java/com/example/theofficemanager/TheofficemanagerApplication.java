e5b46266df2d47b21812b4c9e028a731053b6528
Amber Mattis <amber.mattis2@gmail.com>
2021-02-13 18:58:16 -0600

    first commit

---
+++ src/main/java/com/example/theofficemanager/TheofficemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.theofficemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheofficemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheofficemanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/theofficemanager/TheofficemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.theofficemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheofficemanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
