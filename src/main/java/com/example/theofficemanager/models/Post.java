d1bb9b9a30c047a323f5c6e43ad222f2bdf222a3
Amber Mattis <amber.mattis2@gmail.com>
2021-02-13 21:55:40 -0600

    added a post model to project and created a posts table in the office database

--- src/main/java/com/example/theofficemanager/models/Post.java
+++ src/main/java/com/example/theofficemanager/models/Post.java
@@ -1,11 +1,57 @@
 package com.example.theofficemanager.models;
 
 import javax.persistence.*;
+import javax.persistence.Table;
+
 
 @Entity
-@Table(name = "posts")
+@Table(name="posts")
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 225)
+    private String person;
+
+    @Column(nullable = false, length = 1000)
+    private String quote;
+
+
+    public Post(){}
+
+//    public Post(Long id, String person, String quote){
+//        this.id = id;
+//        this.person = person;
+//        this.quote = quote;
+//
+//    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+
 }
---
+++ src/test/java/com/example/theofficemanager/models/PostTest.java
@@ -0,0 +1,32 @@
+package com.example.theofficemanager.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getPerson() {
+    }
+
+    @Test
+    void setPerson() {
+    }
+
+    @Test
+    void getQuote() {
+    }
+
+    @Test
+    void setQuote() {
+    }
+}
