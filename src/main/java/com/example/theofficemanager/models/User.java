40c29cdb9b7ab0c25d8abd002e81e24887ba739b
Amber Mattis <amber.mattis2@gmail.com>
2021-04-01 19:53:10 -0500

    adding a users table to office manager database

---
+++ src/main/java/com/example/theofficemanager/models/User.java
@@ -0,0 +1,14 @@
+package com.example.theofficemanager.models;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+
+
+}
